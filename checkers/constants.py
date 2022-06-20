3ae2f367d49bef44841f2186b8a124a5e4748ed4
Marco Silva <marcoantonio02016@outlook.com>
2022-06-20 20:43:59 -0300

    Checkers game with Pygame, first and fundations for the game

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+#Width => Largura, Width => Altura
+WIDTH, HEIGHT = 450, 450
+#Rows => Linhas, Cols => Colunas
+ROWS, COLS = 8, 8
+#SQUARE_SIZE => É o resto divisão de largura por Colunas
+SQUARE_SIZE = WIDTH//COLS
+
+#COLOR for the game
+GREY = (186, 186, 186)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (217, 97, 11)
---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+#Here I drawn basic forms geme
+#The FPS const not included in constants because this info is important for me(user) no important for the game
+FPS = 60
+
+#Draw primary window, the vars width and height I get from checkers/constants.py
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+#
+def main():
+    run = True
+    #The clock is for define the game FPS for equalize the for all user PC
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            #Look evets, this event look if user want exit the game
+            if event.type == pygame.QUIT:
+                run = False
+            
+            #Look the event click of the mouse
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quit()
+
+main()
